98470cd3eb3b6d984e3120381ddb7a7644045f0b
ThomasMean <mail@thomasmean.co.uk>
2018-12-03 20:31:33 +0000

    Add nones to input rendering

--- tests/test_gamescreen.py
+++ tests/test_gamescreen.py
@@ -3,10 +3,23 @@ import imp
 gamescreen = imp.load_source("gamescreen","gamescreen.py")
 
 
-def testRender():
+def test_Render():
     assert gamescreen.render(renderInput) == expectedRenderResult;
 
+# def testRenderWithNones():
+#     assert gamescreen.render(renderInput) == expectedNRenderResultWithNones;
+
 expectedRenderResult ="1|2|3|4|5|6|7|8|9\n" \
+    "2|3|4|5|6|7|8|9|1\n" \
+    "3|4|5|6|7|8|9|1|2\n" \
+    "4|5|6|7|8|9|1|2|3\n" \
+    "5|6|7|8|9|1|2|3|4\n" \
+    "6|7|8|9|1|2|3|4|5\n" \
+    "7|8|9|1|2|3|4|5|6\n" \
+    "8|9|1|2|3|4|5|6|7\n" \
+    "9|1|2|3|4|5|6|7|8"
+
+expectedNRenderResultWithNones =" |2|3|4|5|6|7|8|9\n" \
     "2|3|4|5|6|7|8|9|1\n" \
     "3|4|5|6|7|8|9|1|2\n" \
     "4|5|6|7|8|9|1|2|3\n" \
@@ -19,3 +32,4 @@ expectedRenderResult ="1|2|3|4|5|6|7|8|9\n" \
 
 
 renderInput = [[1,2,3,4,5,6,7,8,9],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],[4,5,6,7,8,9,1,2,3],[5,6,7,8,9,1,2,3,4],[6,7,8,9,1,2,3,4,5],[7,8,9,1,2,3,4,5,6],[8,9,1,2,3,4,5,6,7],[9,1,2,3,4,5,6,7,8]]
+renderWithNonesInput = [[None,2,3,4,5,6,7,8,9],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],[4,5,6,7,8,9,1,2,3],[5,6,7,8,9,1,2,3,4],[6,7,8,9,1,2,3,4,5],[7,8,9,1,2,3,4,5,6],[8,9,1,2,3,4,5,6,7],[9,1,2,3,4,5,6,7,8]]
