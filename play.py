68ff6c9e73110280d3ac229f045bd21037ede36b
ThomasMean <mail@thomasmean.co.uk>
2018-12-03 23:11:20 +0000

    Extraction of move checking from main play method

--- play.py
+++ play.py
@@ -4,13 +4,15 @@ gamestate = imp.load_source("","gamestate.py")
 gamescreen = imp.load_source("","gamescreen.py")
 
 def completeGame(state):
-    moveCount = 0
     while not gamestate.gameFinished(state):
         for xValue in range(9):
             for yValue in range(9):
-                if state[xValue][yValue] is None:
-                    move = gamestate.getMoveCheck([xValue, yValue], state)
-                    if move is not None:
-                        state = gamestate.add(move,[xValue, yValue],state)
-                        moveCount += 1
+                checkMove(xValue,yValue,state)
+    return state
+
+def checkMove(xValue,yValue,state):
+    if state[xValue][yValue] is None:
+        move = gamestate.getMoveCheck([xValue, yValue], state)
+        if move is not None:
+            state = gamestate.add(move,[xValue, yValue],state)
     return state
--- tests/test_play.py
+++ tests/test_play.py
@@ -1,6 +1,12 @@
 import imp
 play = imp.load_source("play","play.py")
 
+def test_checkMove_successful():
+    assert play.checkMove(0,0,firstState) == completeState
+
+def test_checkMove_fail():
+    assert play.checkMove(0,1,firstState) == firstState
+
 def test_CompleteGame():
     assert play.completeGame(firstState) == completeState
 
@@ -15,7 +21,7 @@ def test_CompleteGame_isCompleted():
 
 
 
-firstState = [[None,2,3,4,5,6,7,8,9],[4,5,6,7,8,9,1,2,3],[7,8,9,1,2,3,4,5,6],[2,3,4,5,6,7,8,9,1],[5,6,7,8,9,1,2,3,4],[8,9,1,2,3,None,5,6,7],[3,4,5,6,7,8,9,1,None],[6,7,None,9,1,2,3,4,5],[9,1,2,3,4,5,6,7,8]]
+firstState = [[None,2,3,4,5,6,7,8,9],[4,5,6,7,8,9,1,2,3],[7,8,9,1,2,3,4,5,6],[2,3,4,5,6,7,8,9,1],[5,6,7,8,9,1,2,3,4],[8,9,1,2,3,4,5,6,7],[3,4,5,6,7,8,9,1,2],[6,7,8,9,1,2,3,4,5],[9,1,2,3,4,5,6,7,8]]
 completeState = [[1,2,3,4,5,6,7,8,9],[4,5,6,7,8,9,1,2,3],[7,8,9,1,2,3,4,5,6],[2,3,4,5,6,7,8,9,1],[5,6,7,8,9,1,2,3,4],[8,9,1,2,3,4,5,6,7],[3,4,5,6,7,8,9,1,2],[6,7,8,9,1,2,3,4,5],[9,1,2,3,4,5,6,7,8]]
 testState = [[None,None,None,2,6,None,7,None,1],[6,8,None,None,7,None,None,9,None],[1,9,None,None,None,4,5,None,None],[8,2,None,1,None,None,None,4,None],[None,None,4,6,None,2,9,None,None],[None,5,None,None,None,3,None,2,8],[None,None,9,3,None,None,None,7,4],[None,4,None,None,5,None,None,3,6],[7,None,3,None,1,8,None,None,None]]
 testCompleteState = [[4,3,5,2,6,9,7,8,1],[6,8,2,5,7,1,4,9,3],[1,9,7,8,3,4,5,6,2],[8,2,6,1,9,5,3,4,7],[3,7,4,6,8,2,9,1,5],[9,5,1,7,4,3,6,2,8],[5,1,9,3,2,6,8,7,4],[2,4,8,9,5,7,1,3,6],[7,6,3,4,1,8,2,5,9]]
